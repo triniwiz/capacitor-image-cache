fdb0e53d28285c9498db09c9dbc197df9059a7e6
Zachary Keeton <zach.keeton@outlook.com>
2019-11-19 12:49:00 -0500

    add missing web definitions

--- src/core/src/web.ts
+++ src/core/src/web.ts
@@ -10,28 +10,35 @@ export class ImageCachePluginWeb extends WebPlugin implements IImageCache {
     });
   }
 
-    clear(): Promise<{ value: boolean }> {
-        return new Promise(()=>{});
-    }
+  clear(): Promise<{ value: boolean }> {
+    return new Promise(() => { });
+  }
 
-    clearItem(options: { src: string }): Promise<{ value: boolean }> {
-        return new Promise(()=>{
-          console.log(options);
-        });
-    }
+  clearItem(options: { src: string }): Promise<{ value: boolean }> {
+    return new Promise(() => {
+      console.log(options);
+    });
+  }
 
-    hasItem(options: { src: string }): Promise<{ value: boolean }> {
-        return new Promise(()=>{
-            console.log(options);
-        });
-    }
+  hasItem(options: { src: string }): Promise<{ value: boolean }> {
+    return new Promise(() => {
+      console.log(options);
+    });
+  }
 
-    get(options: { src: string, overwrite?: boolean }): Promise<{ value: string }> {
-        return new Promise((resolve)=>{
-            // return default src for now
-            resolve({value:options.src});
-        });
-    }
+  get(options: { src: string, overwrite?: boolean }): Promise<{ value: string }> {
+    return new Promise((resolve) => {
+      // return default src for now
+      resolve({ value: options.src });
+    });
+  }
+
+  saveImage(options: { src: string }): Promise<void> {
+    return new Promise((resolve) => {
+      console.log(options);
+      resolve();
+    });
+  }
 }
 
 const ImageCachePlugin = new ImageCachePluginWeb();
